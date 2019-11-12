5d65e6b3b86a6d70b0fac603d9e8f0c450a3e1e1
thiagoschetini <thiagoschetini@gmail.com>
2019-11-11 23:17:14 -0200

    updating all variables on spark submit builder

--- src/main/scala/br/com/bvs/datalake/model/SparkSubmitBuilder.scala
+++ src/main/scala/br/com/bvs/datalake/model/SparkSubmitBuilder.scala
@@ -1,41 +1,35 @@
 package br.com.bvs.datalake.model
 
 class SparkSubmitBuilder {
-  private val basicFlags = "spark-submit --master yarn --deploy-mode cluster"
-  private val queueFlag = "--queue "
-
+  private val basic = "spark-submit --master yarn --deploy-mode cluster"
+  private val queue = "--queue "
+  private var queueValue: String = _
 
   /* driver */
-  private val driverMemoryFlag = "--driver-memory "
-  private val driverCoresFlag = "--driver-cores "
-  private val driverMemoryOverheadFlag = "--conf spark.yarn.driver.memoryOverhead="
+  private val driverMemory = "--driver-memory "
+  private val driverCores = "--driver-cores "
 
   /* executors */
-  private val numExecutorsFlag = "--num-executors "
-  private val executorMemoryFlag = "--executor-memory "
-  private val executorCoresFlag = "--executor-cores "
-  private val executorMemoryOverheadFlag = "--conf spark.yarn.executor.memoryOverhead="
+  private val numExecutors = "--num-executors "
+  private val executorMemory = "--executor-memory "
+  private val executorCores = "--executor-cores "
+
+  /* over memory */
+  private val offHeapEnable = "--conf spark.memory.offHeap.enabled=true"
+  private val offHeapSize = "--conf spark.memory.offHeap.size="
+  private val driverMemoryOverhead = "--conf spark.yarn.driver.memoryOverhead="
+  private val executorMemoryOverhead = "--conf spark.yarn.executor.memoryOverhead="
 
   /* tunning */
-  private val driverNoLimit = "--conf spark.driver.maxResultSize=0"
+  private val noDriverLimit = "--conf spark.driver.maxResultSize=0"
   private val jvmElasticYoungGen = "--conf spark.executor.extraJavaOptions=\"-XX:+UseG1GC -XX:NewRatio=1 -XX:SurvivorRatio=128 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=5\""
+  private val forceKryoSerializer = "--conf spark.serializer=org.apache.spark.serializer.KryoSerializer"
+  private val shuffleConnections = "--conf spark.shuffle.io.numConnectionsPerPeer="
 
-
+  /* ethernet stability tolerance */
+  private val yarnMaxRetries = "--conf spark.yarn.maxAppAttempts="
+  private val shuffleMaxRetries = "--conf spark.shuffle.io.maxRetries="
 }
-
-
-/*
-
-
---conf spark.shuffle.io.numConnectionsPerPeer=10 \
---conf spark.memory.offHeap.enabled=true \
---conf spark.memory.offHeap.size=16384 \
---conf spark.executor.extraJavaOptions="-XX:+UseG1GC -XX:NewRatio=1 -XX:SurvivorRatio=128 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=5" \
---conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
---conf spark.yarn.maxAppAttempts=5 \
---conf spark.shuffle.io.maxRetries=5 \
- */
-
 
 /*
 spark-submit \
