563128cb340b2e71a8aa557989ef9706fa1f2e0a
thiagoschetini <thiagoschetini@gmail.com>
2019-11-14 15:33:43 -0200

    spark-submit bug fixed

--- src/main/scala/br/com/bvs/datalake/helper/SparkHelper.scala
+++ src/main/scala/br/com/bvs/datalake/helper/SparkHelper.scala
@@ -30,7 +30,7 @@ object SparkHelper {
 
   /* tunning */
   private val resultsFromExecutorsToNoLimits = "spark.driver.maxResultSize=0"
-  private val jvmYoungGenTuning = "spark.executor.extraJavaOptions=\"-XX:+UseG1GC -XX:NewRatio=1 -XX:SurvivorRatio=128 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=5\""
+  private val jvmYoungGenTuning = "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:NewRatio=1 -XX:SurvivorRatio=128 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=5"
   private val useKryoSerializer = "spark.serializer=org.apache.spark.serializer.KryoSerializer"
 
   /* ethernet stability tolerance */
