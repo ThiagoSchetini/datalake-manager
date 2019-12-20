ec2c269c9c16064e868c7b29ca55da0c76be7ab9
thiagoschetini <thiagoschetini@gmail.com>
2019-12-20 16:47:45 -0200

    tests refactor

--- src/test/data/FileToHiveTransaction/prepare.sh
+++ src/test/data/FileToHiveTransaction/prepare.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env bash
 
-title="FileToHiveTransaction test data:"
+title="FileToHiveTransaction:"
 
-#-- local --#
+#-- local paths --#
 test=src/test/data/FileToHiveTransaction
 csv=${test}/file1.csv
 csv2=${test}/file2.csv
 csv3=${test}/file3.csv
-smCSV=${test}/sm-csv.properties
-smDir=${test}/sm-dir.properties
+smSingleCSV=${test}/sm-single-csv.properties
+smMultiCSV=${test}/sm-multi-csv.properties
 
-#-- hdfs --#
+#-- hdfs paths --#
 testHdfs=/br/com/bvs/datalake/transaction/FileToHiveTransaction
-watchCSV=${testHdfs}/watchCSV
-watchDir=${testHdfs}/watchDir
-sourceCSV=${testHdfs}/sourceCSV
-sourceDir=${testHdfs}/sourceDir
+watchSingleCSVSM=${testHdfs}/watchSingleCSVSM
+watchMultiCSVSM=${testHdfs}/watchMultiCSVSM
+sourceSingleCSV=${testHdfs}/sourceSingleCSV
+sourceMultiCSV=${testHdfs}/sourceMultiCSV
 destiny=${testHdfs}/destiny
 
 #-- sanitize hdfs and hive --#
+echo "[DATA] ${title} prepare destiny"
 hdfs dfs -rm -R -skipTrash ${destiny} 2>/dev/null
 hdfs dfs -mkdir -p ${destiny}
 hive -e "drop table if exists testdb.types;"
 
-echo "[TEST] ${title} transact a single csv file"
-hdfs dfs -rm -R -skipTrash ${sourceCSV} 2>/dev/null
-hdfs dfs -rm -R -skipTrash ${watchCSV} 2>/dev/null
-hdfs dfs -mkdir -p ${sourceCSV}
-hdfs dfs -mkdir -p ${watchCSV}
-hdfs dfs -copyFromLocal ${csv} ${sourceCSV}
-hdfs dfs -copyFromLocal ${smCSV} ${watchCSV}
+echo "[DATA] ${title} prepare single csv"
+hdfs dfs -rm -R -skipTrash ${sourceSingleCSV} 2>/dev/null
+hdfs dfs -rm -R -skipTrash ${watchSingleCSVSM} 2>/dev/null
+hdfs dfs -mkdir -p ${sourceSingleCSV}
+hdfs dfs -mkdir -p ${watchSingleCSVSM}
+hdfs dfs -copyFromLocal ${csv} ${sourceSingleCSV}
+hdfs dfs -copyFromLocal ${smSingleCSV} ${watchSingleCSVSM}
 
-echo "[TEST] ${title} transact from a dir with files"
-hdfs dfs -rm -R -skipTrash ${sourceDir} 2>/dev/null
-hdfs dfs -rm -R -skipTrash ${watchDir} 2>/dev/null
-hdfs dfs -mkdir -p ${sourceDir}
-hdfs dfs -mkdir -p ${watchDir}
-hdfs dfs -copyFromLocal ${csv2} ${sourceDir}
-hdfs dfs -copyFromLocal ${csv3} ${sourceDir}
-hdfs dfs -copyFromLocal ${smDir} ${watchDir}
+echo "[DATA] ${title} prepare multi csv"
+hdfs dfs -rm -R -skipTrash ${sourceMultiCSV} 2>/dev/null
+hdfs dfs -rm -R -skipTrash ${watchMultiCSVSM} 2>/dev/null
+hdfs dfs -mkdir -p ${sourceMultiCSV}
+hdfs dfs -mkdir -p ${watchMultiCSVSM}
+hdfs dfs -copyFromLocal ${csv2} ${sourceMultiCSV}
+hdfs dfs -copyFromLocal ${csv3} ${sourceMultiCSV}
+hdfs dfs -copyFromLocal ${smMultiCSV} ${watchMultiCSVSM}
--- src/test/data/SmartContractRanger/prepare.sh
+++ src/test/data/SmartContractRanger/prepare.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-title="SmartContractRanger test data:"
+title="SmartContractRanger:"
 
-#-- local --#
+#-- local paths --#
 test=src/test/data/SmartContractRanger
 sm=${test}/placebo.properties
 
-#-- hdfs --#
+#-- hdfs paths --#
 testHdfs=/br/com/bvs/datalake/core/SmartContractRanger
 watch1=${testHdfs}/watch1
 watch2=${testHdfs}/watch2
 ongoing=${testHdfs}/watch1/ongoing
 
-echo "[TEST] ${title} watch from 2 folders on parallel"
+echo "[DATA] ${title} sm on 2 paths"
 hdfs dfs -rm -R -skipTrash ${watch1} 2>/dev/null
 hdfs dfs -rm -R -skipTrash ${watch2} 2>/dev/null
 hdfs dfs -mkdir -p ${watch1}
@@ -20,7 +20,7 @@ hdfs dfs -mkdir -p ${watch2}
 hdfs dfs -copyFromLocal ${sm} ${watch1}
 hdfs dfs -copyFromLocal ${sm} ${watch2}
 
-echo "[TEST] ${title} look for sm on ongoing dir"
+echo "[DATA] ${title} sm on ongoing"
 hdfs dfs -rm -R -skipTrash ${ongoing} 2>/dev/null
 hdfs dfs -mkdir -p ${ongoing}
 hdfs dfs -copyFromLocal ${sm} ${ongoing}
